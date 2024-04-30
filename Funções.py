4ed52bcceb642e03b6863882eae6ce47806618fe
Andre <andrefd1@al.insper.edu.br>
2024-04-30 17:21:06 -0300

    Primeira_Funcao

--- Funções.py
+++ Funções.py
@@ -1,2 +1,16 @@
-teste = 'vini goat'
-teste_2="Davi "
+#Primeira Funçao: Cria matriz quadrada de espaços
+def cria_mapa(N):
+
+    mapa = []
+
+    for i in range(N):
+        
+        linha = []
+
+        for j in range(N):
+            
+            linha.append(' ')
+
+        mapa.append(linha)
+
+    return mapa
