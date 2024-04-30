98417dfc4e8844ab538e64d4843f42e2eb1031b4
Andre <andrefd1@al.insper.edu.br>
2024-04-30 17:25:09 -0300

    Segunda_funcao

--- Funções.py
+++ Funções.py
@@ -14,3 +14,26 @@ def cria_mapa(N):
         mapa.append(linha)
 
     return mapa
+
+#Segunda Funçao: Navio pode ser alocado na posição?
+def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
+    n = len(mapa)  
+
+    if orientacao == 'v':  
+        
+        if linha + blocos > n:
+            return False
+        
+        for i in range(blocos):
+            if mapa[linha + i][coluna] != ' ':
+                return False
+
+    elif orientacao == 'h':  
+        if coluna + blocos > n:
+            return False
+        
+        for j in range(blocos):
+            if mapa[linha][coluna + j] != ' ':
+                return False
+
+    return True  
