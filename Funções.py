d4226c6285d00a95b4a0e44b1617e48bbc2bf361
Lovizio <davifl@al.insper.edu.br>
2024-04-30 17:35:27 -0300

    Terceira Função

--- Funções.py
+++ Funções.py
@@ -1,3 +1,5 @@
+import random
+
 #Primeira Funçao: Cria matriz quadrada de espaços
 def cria_mapa(N):
 
@@ -37,3 +39,40 @@ def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
                 return False
 
     return True  
+
+#Terceira Função
+
+def navio_pode_ser_alocado(mapa, navio_tamanho, linha, coluna, orientacao):
+    n = len(mapa)
+    if orientacao == 'h':
+        if coluna + navio_tamanho > n:
+            return False
+        for j in range(coluna, coluna + navio_tamanho):
+            if mapa[linha][j] != ' ':
+                return False
+    else:  
+        if linha + navio_tamanho > n:
+            return False
+        for i in range(linha, linha + navio_tamanho):
+            if mapa[i][coluna] != ' ':
+                return False
+    return True
+
+def aloca_navios(mapa, blocos):
+    n = len(mapa)
+    for navio_tamanho in blocos:
+        alocado = False
+        while not alocado:
+            linha = random.randint(0, n-1)
+            coluna = random.randint(0, n-1)
+            orientacao = random.choice(['h', 'v'])
+            
+            if navio_pode_ser_alocado(mapa, navio_tamanho, linha, coluna, orientacao):
+                if orientacao == 'h':
+                    for j in range(coluna, coluna + navio_tamanho):
+                        mapa[linha][j] = 'N'
+                else:  
+                    for i in range(linha, linha + navio_tamanho):
+                        mapa[i][coluna] = 'N'
+                alocado = True
+    return mapa
